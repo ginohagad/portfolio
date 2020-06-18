5ef4722cc483cbfe0f1572cfa0bd0a5116de3305
GH <ginohagad@gmail.com>
2020-06-18 21:37:20 +1200

    Trying to import images using an array - not working yet.

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {Page} from '../';
+import images from '../../images';
 
 const Skills = () => {
 
   return (
     <Page title="Skills">
-      <p>This is the Skills page.</p>
+      <div className="card horizontal">
+        {images.map(({id, src, title}) => 
+          <div className="card-image">
+            <img className="web_logos" src={src} alt="HTML Logo" />
+            <span className="card-title">{title}</span>
+          </div>
+        )}
+      </div>
     </Page>
   )
 }
---
+++ src/images.js
@@ -0,0 +1,7 @@
+const images = [
+  { id: 1, src: 'html5.png', title: 'HTML5' },
+  { id: 2, src: 'css3.png', title: 'CSS3' },
+];
+
+
+export default images;
