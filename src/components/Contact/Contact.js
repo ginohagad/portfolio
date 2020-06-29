552749e03bccfe13c40e3db4c0ba679eded76215
Gino <ginohagad@gmail.com>
2020-06-30 11:45:11 +1200

    Added padding to About and Contact page.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,12 +6,12 @@ import {Page} from '../'
 const Contact = () => {
 	return (
     <Page title="Contact">
-			<div>
+			<div className="contact">
 				<p>	
 					<a aria-label="Email Gino" href="mailto:ginohagad@gmail.com" data-position="top" data-tolltop="Email Gino" className="btn-floating btn-large waves-effect waves-light brown tooltipped">
 						<i className="fa fa-envelope"></i>
 					</a>
-					<a aria-label="Email Gino" href="mailto:ginohagad@gmail.com" className="hoverline">ginohagad@gmail.com</a>
+					<a aria-label="Email Gino" href="mailto:ginohagad@gmail.com" className="email">ginohagad@gmail.com</a>
 				</p>
 			</div>
     </Page>
