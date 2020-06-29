94607c9a3a3b93233bd94948f35c3572e617ea53
Gino <ginohagad@gmail.com>
2020-06-30 08:39:18 +1200

    Updated About page.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
 
    const about_content = {
-      caption: '"About Gino Hagad"',
+      caption: '"About Me"',
       content: 'I am a freelance Front-end Web Developer and a Bachelor of Information and Communications Technology graduate based in Wellington City, New Zealand. I build websites and web apps that are fast, easy to use and coded with the same eye for detail as a designer.',
     };
 
